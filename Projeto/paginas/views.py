4458338ca11af2099f1fa9bc58a34762e3205fb6
Raquel Eucaria <raqueleucaria@gmail.com>
2023-10-30 21:42:38 -0300

    delete projeto cancelado

--- Projeto/paginas/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-
-# Create your views here.
-class IndexView(TemplateView):
-    template_name = "paginas/modelo.html"
-
-class GradeView(TemplateView):
-    template_name = "paginas/grade.html"
